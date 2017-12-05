469b33cf1cc2f4ae393ed32387aae3f8b6fd20e5
Oleksii Yaryi <oleksii.yaryi@gmail.com>
2017-12-05 17:37:13 +0200

    add SetWebHook commande

---
+++ src/Console/Commands/ViberSetWebHook.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace BotMan\Drivers\Viber\Commands;
+
+use Illuminate\Console\Command;
+
+class Nlp extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'botman:viber-webhook';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Set webhook for Viber API';
+
+	/**
+	 * Create a new command instance.
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function handle()
+	{
+		$callbackUrl = $this->ask('What is the target url for the Viber bot?');
+		$api = 'https://chatapi.viber.com/pa/set_webhook';
+
+		$postdata = json_encode(
+			[
+				"url"         => $callbackUrl,
+				"event_types" => [
+					"delivered",
+					"seen",
+					"failed",
+					"subscribed",
+					"unsubscribed",
+					"conversation_started"
+				]
+			]
+		);
+
+		$opts = array(
+			'http' =>
+				array(
+					'method'  => 'POST',
+					'header'  => [
+						'Content-type: application/x-www-form-urlencoded;',
+						'X-Viber-Auth-Token: ' . config('botman.viber.token'),
+					],
+					'content' => $postdata
+				)
+		);
+
+		$context = stream_context_create($opts);
+
+		$result = json_decode(file_get_contents($api, false, $context));
+
+		if (isset($result['status']) && $result['status'] === 0) {
+			$this->info('Your bot is now set up with Viber\'s webhook!');
+		}
+		else {
+			$this->error(
+				'Something went wrong!Status: ' . $result['status'] . '. Message:' . $result['status_message']
+			);
+		}
+	}
+}
